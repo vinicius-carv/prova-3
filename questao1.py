cfa7b324648c75a83a0a814552a1e63f9d17eb53
vinicius-carv <valves8956@gmail.com>
2021-06-11 23:48:58 -0300

    Atualizando a minha branch

--- questao1.py
+++ questao1.py
@@ -1,5 +1,16 @@
 """
 Ciência da Computação - 3º Semestre
+
+1. Terceira verificação de aprendizagem (projeto).
+2. Instruções do desenvolvimento do projeto para avaliação:
+- Fazer em grupo de até cinco pessoas, mas a apresentação será individual.
+- Prazo: enviar dia 16/06/20 até 16h.
+- Apresentação: dia 17/06/20 no Google Meet no horário da aula.
+- Coloque o nome de todos os alunos do grupo dentro de cada programa ‘.py’
+- Apenas um aluno do grupo deve enviar o projeto pela atividade do Classroom em um arquivo
+“.zip” (aluno1_aluno2_aluno3_aluno4.zip) com todos os arquivos desenvolvidos:
+Um arquivo projeto1 “.py”; e
+Um arquivo projeto2 “.py”.
 3. Projeto 1 (POO):
 Elabore o enunciado de um problema que será resolvido com POO, herança e classe abstrata.
 O projeto deve ter no mínimo:
@@ -19,16 +30,19 @@ class User(ABC):
     def Login(self):
         pass
 class Paciente(User):
-    def __init__(self,login,senha,nome,endereco=None,exame=[]):
+    def __init__(self,login,senha,nome,endereco=None,registros=0):
         self.login=login
         self.senha=senha
         self.nome=nome
+        self.exame=list()
         self.endereco=endereco
+        self.registros=registros
     def __str__(self):
         s=f"Cliente: {self.nome}\nLogin:{self.login}\nEndereço atual: {self.endereco}"
         return s
     def Login(self,login,senha):
         log=False
+        print(self.login,"\n")
         if self.login==login:
             if self.senha==senha:
                 log=True
@@ -41,7 +55,7 @@ class Paciente(User):
             return True
         else:
             print("\nDado Inválido, tente novamente\n")
-            return False
+            return False    
     def get_nome(self):
         return self.nome
     def get_endereco(self):
@@ -50,8 +64,58 @@ class Paciente(User):
         self.endereco = new
     def set_nome(self,new):
         self.nome = new
+    def marcar_exame(self):
+        print(f"Olá, {self.nome}\nPainel De Marcação de Exame")
+        tipo = input("Tipo do Exame: ")
+        data = input("\nInsira a data(aaaa-mm-dd): ")
+        medico = input("\nInsira o nome Médico: ")
+        marcado=[tipo,data,medico]
+        self.exame.append(marcado)
+        self.registros+=1
     def ver_exame(self):
-        ...
+        i=0
+        print(f"Histórico de exames do paciente {self.nome}:")
+        while i<self.registros:
+            print("\n--------------------------------")
+            print(f"Exame nº{i+1}")
+            print("Tipo de exame:",self.exame[i][0])
+            print("\nData do exame:",self.exame[i][1])
+            print("\nMédico responsável pelo exame:",self.exame[i][2])
+            print("\n--------------------------------")
+            i+=1
+        print("\n")
+    def cancelar_exame(self):
+        Paciente.ver_exame(self)
+        op=int(input("Selecione, da lista, o nº do exame que deseja remover:\n"))
+        confirm=self.exame[op-1]
+        while True:
+            confmesg=input(f"Deseja cancelar {confirm}?\n[Y]/[N]\n").upper()
+            if confmesg=="Y":
+                self.exame.pop(op-1)
+                print("Operação concluída")
+                break
+            elif confmesg=="N":
+                print("Operação cancelada")
+                break
+            else:
+                print("Opção inválida")
+    def remarcar_exame(self):
+        Paciente.ver_exame(self)
+        op=int(input("Selecione, da lista, o nº do exame que deseja remarcar:\n"))
+        confirm=self.exame[op-1]
+        while True:
+            confmesg=input(f"Deseja remarcar {confirm}?\n[Y]/[N]\n").upper()
+            if confmesg=="Y":
+                newDate=input("Digite a nova data(aaaa-mm-dd):\n")
+                self.exame[op-1][1]=newDate
+                print(self.exame[op-1])
+                print("Operação concluída")
+                break
+            elif confmesg=="N":
+                print("Operação cancelada")
+                break
+            else:
+                print("Opção inválida")
 class Medico(User):
     def __init__(self,login,senha,nome):
         self.login=login
@@ -73,6 +137,10 @@ if __name__ == "__main__":
     #Login do CLIENTE é o CPF
     #Login do MEDICO é o CRM
     c1=Paciente(12345678912,112233,"João Azevedo","apt 404, Edifício Araucárias, QR410, Brasilia, DF")
-    a=input("Login: ")
-    b=input("Senha: ")
-    Paciente.Login(c1,a,b)
+    #a=input("Login: ")
+    #b=input("Senha: ")
+    #Paciente.Login(c1,a,b)
+    c1.marcar_exame()
+    c1.marcar_exame()
+    c1.ver_exame()
+    c1.remarcar_exame()
